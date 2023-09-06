becc8e4fcfac193748e75a8038255d2644da1789
dhxh <dhxh@dhxh.com>
2023-09-06 10:19:20 +0800

    update k3 build

--- build/immortalwrt_phicomm_k3/diy-2.sh
+++ build/immortalwrt_phicomm_k3/diy-2.sh
@@ -3,7 +3,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.100.20/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' package/lean/default-settings/files/zzz-default-settings
@@ -11,5 +11,9 @@ sed -i 's/192.168.1.1/192.168.100.20/g' package/base-files/files/bin/config_gene
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 编译K3的时候会出很多其他牌子路由器固件的，这个代码使用之后只出K3固件
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
+
+#修复k3屏幕
+# mv -f "${PATH1}"/k3screenctrl/000-k3screen.patch feeds/packages/utils/phicomm-k3screenctrl/patches
+# mv "${PATH1}"/k3screenctrl/906-BCM5301x-uart1.patch target/linux/bcm53xx/patches-5.4/906-BCM5301x-uart1.patch
